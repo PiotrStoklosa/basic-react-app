df759246a1b767fc9368b91c09b186367630beb5
PiotrStoklosa <piotrjstoklosa@gmail.com>
2022-03-16 12:34:14 +0100

    Initial commit

---
+++ src/HelloWorld.tsx
@@ -0,0 +1,12 @@
+import React from "react";
+
+class HelloWorld extends React.Component {
+    render() {
+        return (
+        <div>
+            <p style={{display: "inline-block"}}>Hello&nbsp;</p><p style={{display: "inline-block"}}> World!</p>
+        </div>);
+    }
+}
+
+export default HelloWorld;
